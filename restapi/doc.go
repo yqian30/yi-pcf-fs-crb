51841fa49f86a3884412ba19084696091a75183f
Amar Karmilla <amar.karmilla@emc.com>
2017-09-19 15:08:35 -0400

    crb implementation, manifest and godeps

---
+++ restapi/doc.go
@@ -0,0 +1,26 @@
+/*Package restapi pcf-fs-crb API
+
+Crb-fs file-system Copy Service Broker provides a way to copy(store) data-stream associated with a copyId to a file-system and retrieve data-stream for a given copyId
+
+
+    Schemes:
+      http
+    Host: localhost:8080
+    BasePath: /crb
+    Version: 0.1
+
+    Consumes:
+    - application/json
+
+    - application/octet-stream
+
+
+    Produces:
+    - application/json
+
+    - application/octet-stream
+
+
+swagger:meta
+*/
+package restapi
